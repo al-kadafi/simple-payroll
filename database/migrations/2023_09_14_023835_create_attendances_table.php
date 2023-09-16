1c2e5b1f91cbc281cde5366c80675e2d531ca17b
Achmad Jafar Al Kadafi <al.kadafi20@gmail.com>
2023-09-16 15:15:14 +0700

    Create attendance and overtime migration

---
+++ database/migrations/2023_09_14_023835_create_attendances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('employee_id');
+            $table->time('start_time')->default('07:30:00');
+            $table->time('end_time')->default('16:30:00');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
---
+++ database/migrations/2023_09_14_023938_create_overtimes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOvertimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('overtimes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('employee_id');
+            $table->datetime('start_time');
+            $table->datetime('end_time');
+            $table->text('reason')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('overtimes');
+    }
+}
