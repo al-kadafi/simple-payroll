e2a676edb393eaabe29663c5487c15a05ab03b0f
Achmad Jafar Al Kadafi <al.kadafi20@gmail.com>
2023-09-16 15:14:18 +0700

    Add relation and assesor function

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,10 +2,51 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Employee extends Model
 {
-    use HasFactory;
+    use SoftDeletes, HasFactory;
+
+    protected $fillable = ['id'];
+
+    public function getWorkingPeriodAttribute()
+    {
+        $currentDate = new \DateTime();
+        $join = new \DateTime($this->join_date);
+
+        // Calculate the difference
+        $interval = $currentDate->diff($join);
+
+        // Access the difference in years, months, and days
+        $years = $interval->y;
+        $month = $interval->m;
+        $day = $interval->d;
+
+        if ($years > 0) {
+            $years = $years . ($years == 1 ? ' Year ':' Years ');
+        } else {
+            $years = '';
+        }
+
+        if ($month > 0) {
+            $month = $month . ($years == 1 ? ' Month ':' Months ');
+        } else {
+            $month = $day . ($years == 1 ? ' Day ':' Days ');
+        }
+
+        return $years . $month;
+    }
+
+    public function attendance()
+    {
+        return $this->hasMany('App\Models\Attendance', 'employee_id');
+    }
+
+    public function overtime()
+    {
+        return $this->hasMany('App\Models\Overtime', 'employee_id');
+    }
 }
