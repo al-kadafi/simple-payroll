98272af0ecdda13cc988fcc621176d3057abb721
Achmad Jafar Al Kadafi <al.kadafi20@gmail.com>
2023-09-17 11:45:30 +0700

    Create slip migration

---
+++ database/migrations/2023_09_16_110927_create_slips_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slips', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('employee_id');
+            $table->string('month_period');
+            $table->enum('status', ['approved', 'rejected','draft']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slips');
+    }
+}
