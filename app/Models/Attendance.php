8476782d3ce28d3036ed4f55724e3e60eb9377ac
Achmad Jafar Al Kadafi <al.kadafi20@gmail.com>
2023-09-17 11:44:43 +0700

    Add mutator

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -3,16 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Attendance extends Model
 {
 
-    protected $fillable = [
-        'id',
-    ];
+    protected $guarded = [];
 
     public function employee()
     {
         return $this->belongsTo('App\Models\Employee', 'employee_id');
+    }
+
+    public function setDateAttribute($value)
+    {
+        $this->attributes['date'] = Carbon::createFromFormat('d F Y', $value)->format('Y-m-d');
     }
 }
