4bcb76f0cf426d6a825d1cd50c8595676ade2cb7
Achmad Jafar Al Kadafi <al.kadafi20@gmail.com>
2023-09-17 11:43:28 +0700

    Add create and edit flow

--- app/Http/Controllers/Dashboard/EmployeeController.php
+++ app/Http/Controllers/Dashboard/EmployeeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Employee;
-use App\Models\Attendance;
+use App\Models\Slip;
 use Illuminate\Support\Facades\Validator;
 
 class EmployeeController extends Controller
@@ -19,7 +19,106 @@ class EmployeeController extends Controller
      */
     public function index(Request $request)
     {
-        $employees = Employee::all();
+        $employees = Employee::oldest()->get();
+
         return view('dashboard.employee', ['employees' => $employees]);
+    }
+
+    /**
+     * show employee
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function show($id)
+    {
+        $employee = Employee::find($id);
+        $slips = Slip::oldest()->get();
+
+        return view('dashboard.profile', ['employee' => $employee, 'slips' => $slips]);
+    }
+
+    /**
+     * store overtime data
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'birth_place' => 'required',
+            'birth_date' => 'required',
+            'gender' => 'required',
+            'position' => 'required',
+            'status' => 'required',
+            'basic_salary' => 'required',
+            'allowance' => 'required',
+            'join_date' => 'required',
+            'insurance' => 'required',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            foreach ($validator->errors()->toArray() as $err) {
+                $message = implode(', ', $err);
+                break;
+            }
+
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with('message', [
+                    'title' => 'Error',
+                    'type' => 'error',
+                    'msg' => $message,
+                ]);
+        }
+
+        $data = $request->except('_token', 'id');
+
+        Employee::updateOrCreate(['id' => $request->id], $data);
+
+        return redirect()
+            ->back()
+            ->with('message', [
+                'title' => 'Success',
+                'type' => 'success',
+                'msg' => 'Employee data successfully ' . ($request->id ? 'edited' : 'added'),
+            ]);
+    }
+
+    /**
+     * delete overtime data
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function delete($id)
+    {
+        $employee = Employee::where('id', decrypt($id))->first();
+
+        if (empty($employee)) {
+            return redirect()
+                ->back()
+                ->with('message', [
+                    'title' => 'Error',
+                    'type' => 'error',
+                    'msg' => 'Employee data not found',
+                ]);
+        }
+
+        $employee->delete();
+
+        return redirect()
+            ->route('employee')
+            ->with('message', [
+                'title' => 'Success',
+                'type' => 'success',
+                'msg' => 'Employee data successfully deleted',
+            ]);
     }
 }
