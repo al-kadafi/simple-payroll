d8fafbcbd30159ef11dbb8b61198f6f7d7fbda36
Achmad Jafar Al Kadafi <al.kadafi20@gmail.com>
2023-09-18 10:37:18 +0700

    Add softdelete helper

--- app/Models/Slip.php
+++ app/Models/Slip.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Slip extends Model
 {
+    use SoftDeletes;
+
     protected $guarded = [];
     
     public function employee()
