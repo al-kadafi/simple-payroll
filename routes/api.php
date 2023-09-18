5f7236ad7dbfe4814a680ac66ab3d4de279bd2f6
Achmad Jafar Al Kadafi <al.kadafi20@gmail.com>
2023-09-18 10:39:53 +0700

    Add api route for get absent

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AttendanceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth.basic')->get('/get-absent-days', [AttendanceController::class, 'getAbsentDays']);
