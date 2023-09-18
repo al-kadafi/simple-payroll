5e5d468e44fc722e784e0e5a118af12a21e14078
Achmad Jafar Al Kadafi <al.kadafi20@gmail.com>
2023-09-18 12:20:43 +0700

    Add deformat helper

--- app/Helpers/basic.php
+++ app/Helpers/basic.php
@@ -1,5 +1,11 @@
 <?php
-    function currency_format($value, $use_symbol = true){
-        return ($use_symbol ? 'Rp. ' : '') . number_format($value, 0, ',', '.');
-    }
+function currency_format($value, $use_symbol = true)
+{
+    return ($use_symbol ? 'Rp. ' : '') . number_format(intval($value), 0, ',', '.');
+}
+
+function deformat_currency($value)
+{
+    return str_replace('Rp', '', str_replace('.', '', $value));
+}
 ?>
